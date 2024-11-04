590ec7abc2f8a1400ccc6d8fee53e838e29da3da
Akash Vishwakarma <akavishwakarma82@gmail.com>
2024-11-04 19:25:39 +0530

    Student Management System

---
+++ src/akash/studentapp/StudentDaoIntrf.java
@@ -0,0 +1,13 @@
+package akash.studentapp;
+
+
+
+public interface StudentDaoIntrf {
+    public void createStudent(Student stu);
+    public void showAllStudent();
+    public void showStudentBasedOnId(String stuid);
+    public void updateStudent(String id, String fname);
+    public void deleteStudent(String id);
+
+
+}
